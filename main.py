e614e995854e15584cae7837143e827513e0be7d
BenG49 <ben@goldfisher.com>
2023-11-20 22:24:21 -0500

    Clean up main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from expr import Expr
 from ruleset import RuleSet
-from op import Op
 from parse import parse
 
 def test(e, expected):
@@ -27,58 +25,12 @@ def canonicalization_test():
 
 	print(e, e == f)
 
-def deriv_test():
-	e = Expr(Op.DERIV,
-		parse('x'),
-		parse('x*x'))
-
-	e = Expr(Op.DERIV,
-		parse('x'),
-		parse('(3x)/x^2'))
-
-	e = Expr(Op.DERIV,
-		parse('x'),
-		parse('3*x^2+x'))
-
-	print(e, '=', e.simplify())
-
-def simplify_test():
-	e = parse('2*x^2+x^2/2')
-	e = parse('x^2/x')
-	e = parse('2*x^3*x')
-
-	# (2*x*y)+(4*3*z*y)+(x*z*1*2)+x = (2*x*y)+(12*y*z)+(2*x*z) = 2x(y+z)+12yz = 2(x(y+z)+6yz)
-	e = parse('(2*x*y)+(4*3*z*y)+(x*z*1*2)+x')
-	
-	print(e, '=', e.simplify())
-
-def distributive_test():
-	e = parse('(3*x)^2')
-
-	e = parse('(2*x)+(3*x)')
-
-	print(e, '=', e.simplify())
-
 def rule_test():
-	e = parse('x+x+0')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-	e = parse('x*(x+0)')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-	e = parse('(3*y^2)+0')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-	e = parse('x+0+0')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-	e = parse('1*(x+0)')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-	e = parse('x^1*x^2')
-	print(e, '->', RuleSet.repeat_apply(e))
-
-def simplify_rule_test():
+	test(parse('x+x+0'), parse('2*x'))
+	test(parse('(3*y^2)+0'), parse('3*y^2'))
+	test(parse('x+0+0'), parse('x'))
+	test(parse('1*(x+0)'), parse('x'))
+	test(parse('x^1*x^2'), parse('x^3'))
 	test(parse('x^2*2+x^2/2'), 	parse('x^2*2+x^2/2'))
 	test(parse('x^2/x'), 		parse('x'))
 	test(parse('2*x^3*x'), 		parse('2*x^4'))
